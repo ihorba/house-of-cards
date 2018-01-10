7d3f5f178278d70bad7fc9462e0eea21b35e37b2
ihor@footballaddicts.com <ihor@footballaddicts.com>
2018-01-10 06:40:07 +0200

    first

---
+++ js/main.js
@@ -0,0 +1,16 @@
+;(function() {
+	"use strict";
+	// window.onload = function(e){
+
+	// var card = document.querySelector('.ba-card').innerHTML;
+	// var cards = document.querySelector('.ba-cards');
+	// var cardsNumber = 36;
+	// console.log(cards);
+	// for(var i = 1; i <= cardsNumber - 1; i++){
+	// 	cards.innerHTML += card;
+	// }
+
+	// };
+
+})();
+
